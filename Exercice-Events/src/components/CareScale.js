9eb5f2d7481110405e80797c94f9031a38eec729
Corentin <corentin01.ressort@gmail.com>
2024-08-05 17:45:44 +0200

    Fin Exercice Event

--- Exercice-Events/src/components/CareScale.js
+++ Exercice-Events/src/components/CareScale.js
@@ -1,6 +1,24 @@
 import Sun from '../assets/sun.svg'
 import Water from '../assets/water.svg'
 
+function need(scaleVal)
+{
+		let val = "";
+		switch(scaleVal){
+			case (1):
+				val = "peu";
+				break;
+			case(2):
+				val = "modérément";
+				break;
+			case(3):
+				val = "beaucoup";
+				break;
+			default:
+			}
+			return (val);
+}
+
 function CareScale({ scaleValue, careType }) {
 	const range = [1, 2, 3]
 	const scaleType =
@@ -11,7 +29,9 @@ function CareScale({ scaleValue, careType }) {
 		)
 
 	return (
-		<div>
+		<div onClick={(e)=>{
+			e.stopPropagation();
+			alert(`Cette Element requiert ${need(scaleValue)} ${careType === "light" ? 'de lumière' : "d'arrosage"}`)}}>
 			{range.map((rangeElem) =>
 				scaleValue >= rangeElem ? (
 					<span key={rangeElem.toString()}>{scaleType}</span>
--- Exercice-Events/src/components/Footer.js
+++ Exercice-Events/src/components/Footer.js
@@ -1,15 +1,30 @@
 import { useState } from 'react'
 import '../styles/Footer.css'
 
+
 function Footer() {
 	const [inputValue, setInputValue] = useState('')
+	
+	function handleInput(e)
+	{
+		setInputValue(e.target.value);
+	}
 
+	function handleBlur()
+	{
+		if (!inputValue.includes("@"))
+		{
+			alert("Votre adresse mail n'est pas valide");
+		}
+	}
 	return (
 		<footer className='lmj-footer'>
 			<div className='lmj-footer-elem'>
 				Pour les passionné·e·s de plantes 🌿🌱🌵
 			</div>
-			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+			<div className='lmj-footer-elem' >Laissez-nous votre mail :
+				<input type='text' value={inputValue} onChange={handleInput} onBlur={handleBlur}/>	
+			</div>
 		</footer>
 	)
 }
